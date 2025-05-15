b401bc7a1ebad8315be5c1723209f9cc4065fa59
Nayem Siddiki <nayemsiddiki2002@gmail.com>
2025-05-16 02:55:01 +0600

    Add files via upload

---
+++ F/F_Digits_Summation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    long long N, M;
+    cin>>N>>M;
+
+    long long x, y;
+    x=N%10;
+    y=M%10;
+
+    cout<<x+y<<endl;
+
+    return 0;
+
+
+}
